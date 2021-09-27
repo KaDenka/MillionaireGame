ff4956c130e6a49377baaa356874cd909d3ca485
Denis <recmanus@gmail.com>
2021-09-27 14:48:17 +0300

    Lesson2_HomeWork_Step2
    Made the SettingsController design finally. Add the logic of the game difficulty transfer to Game Screen.

---
+++ MillionaireGame/MillionaireGame/ClassesAndStructuresSource/GameDifficulty.swift
@@ -0,0 +1,8 @@
+//
+//  GameDifficulty.swift
+//  MillionaireGame
+//
+//  Created by Denis Kazarin on 27.09.2021.
+//
+
+import Foundation
---
+++ MillionaireGame/MillionaireGame/Strategy/GameDifficultyStrategy.swift
@@ -0,0 +1,8 @@
+//
+//  StraightStrategy.swift
+//  MillionaireGame
+//
+//  Created by Denis Kazarin on 27.09.2021.
+//
+
+import Foundation
